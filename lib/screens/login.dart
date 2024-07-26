9aa4d1dc1918c3d228c1fb52c1ea05fae01f865f
TerraBean <mujahidadams@ymail.com>
2024-07-26 11:01:49 +0000

    - refactor login screen with form validation - remove adminSelected parameter - update UI layout and styling

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -14,92 +14,176 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
   String _username = '';
   String _password = '';
   String _loginMessage = '';
   String _userId = '';
-  bool adminSelected = false; // Declare adminSelected as a state variable
+  bool adminSelected = false; // Removed this variable
 
-  Future<void> _handleLogin(bool adminSelected) async {
-    try {
-      final loginResponse = adminSelected
-          ? await AdminApi.loginAdmin(_username, _password)
-          : await UserApi.loginUser(_username, _password);
-      setState(() {
-        _loginMessage = loginResponse.message;
-        _userId = loginResponse.userId;
+  Future<void> _handleLogin() async { // Removed adminSelected parameter
+    if (_formKey.currentState!.validate()) {
+      try {
+        // Assuming you have a way to determine if the user is an admin
+        // based on the username or other criteria.
+        bool isAdmin = _username == 'admin'; // Example check
 
-        switch (_loginMessage) {
-          case 'Login was a successful':
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    HomeScreen(username: _username, userId: _userId),
-              ),
-            );
-            break;
-          case 'Admin login successful':
-            Navigator.pushReplacement(
+        final loginResponse = isAdmin
+            ? await AdminApi.loginAdmin(_username, _password)
+            : await UserApi.loginUser(_username, _password);
+        setState(() {
+          _loginMessage = loginResponse.message;
+          _userId = loginResponse.userId;
+
+          switch (_loginMessage) {
+            case 'Login was a successful':
+              Navigator.pushReplacement(
                 context,
                 MaterialPageRoute(
-                  builder: (context) =>  Admin(),
-                ));
-            break;
-          default:
-
-            break;
-        }
-
-  
-      });
-    } on Exception catch (error) {
-      setState(() {
-        _loginMessage = error.toString();
-      });
+                  builder: (context) =>
+                      HomeScreen(username: _username, userId: _userId),
+                ),
+              );
+              break;
+            case 'Admin login successful':
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Admin(),
+                  ));
+              break;
+            default:
+              break;
+          }
+        });
+      } on Exception catch (error) {
+        setState(() {
+          _loginMessage = error.toString();
+        });
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Login'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: const InputDecoration(labelText: 'Username'),
-              onChanged: (value) => setState(() => _username = value),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                const SizedBox(height: 30),
+                Text(
+                  'Welcome Back!',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Login to continue',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Username',
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide.none,
+                      ),
+                      prefixIcon: Icon(Icons.person, color: Colors.grey),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your username';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) => setState(() => _username = value),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide.none,
+                      ),
+                      prefixIcon: Icon(Icons.lock, color: Colors.grey),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) => setState(() => _password = value),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _handleLogin, // Call _handleLogin without adminSelected
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        padding: EdgeInsets.symmetric(vertical: 18),
+                        textStyle: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: Text(
+                    _loginMessage,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 10.0),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              onChanged: (value) => setState(() => _password = value),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () =>
-                  _handleLogin(adminSelected), // Pass adminSelected
-              child: const Text('Login'),
-            ),
-            // generate a check box which when checked will make adminSelected becomes true and vice versa
-            Checkbox(
-              value: adminSelected, // Bind value to adminSelected
-              onChanged: (value) {
-                setState(() {
-                  adminSelected = value!;
-                  // print the value of adminSelected
-                });
-              },
-            ),
-            const Text('Admin'),
-            const SizedBox(height: 10.0),
-            Text(_loginMessage),
-          ],
+          ),
         ),
       ),
     );

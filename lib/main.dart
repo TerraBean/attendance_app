9237894fe6aa54242ae04e344f3511112f459578
TerraBean <mujahidadams@ymail.com>
2024-07-26 11:38:49 +0000

    Add Firebase configuration and plugins to Android, macOS, and Windows platforms

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@
 import 'package:attendance_app/screens/login.dart';
 import 'package:attendance_app/services/location_services.dart';
 import 'package:attendance_app/services/radius_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-void main() {
+import 'firebase_options.dart';
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
